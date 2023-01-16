20a5b0ed3c7933ee3748b990982f1214af6e0485
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-16 17:41:03 -0300

    :recycle:  refactor: updating methods

--- src/main/java/br/com/geradordedevs/financialnotification/services/SpreadSheetService.java
+++ src/main/java/br/com/geradordedevs/financialnotification/services/SpreadSheetService.java
@@ -3,11 +3,9 @@ package br.com.geradordedevs.financialnotification.services;
 import br.com.geradordedevs.financialnotification.entites.SpreadSheetEntity;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.InputStream;
-import java.util.List;
-
 public interface SpreadSheetService {
 
-    boolean validateExcelFile(MultipartFile file);
-    List<SpreadSheetEntity> getSpreadSheetFromExcel(InputStream inputStream);
+    SpreadSheetEntity saveSpreadSheetToDatabase(MultipartFile file);
+
+    Iterable<SpreadSheetEntity> getCustomers();
 }
