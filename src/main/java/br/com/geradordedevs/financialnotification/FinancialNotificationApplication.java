3efd202db106c62cbf460ca43daba17e7563714c
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-16 17:02:27 -0300

    :recycle:  refactor: addint anotatiton SpringBootApplication

--- src/main/java/br/com/geradordedevs/financialnotification/FinancialNotificationApplication.java
+++ src/main/java/br/com/geradordedevs/financialnotification/FinancialNotificationApplication.java
@@ -2,8 +2,9 @@ package br.com.geradordedevs.financialnotification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class FinancialNotificationApplication {
 
 	public static void main(String[] args) {
