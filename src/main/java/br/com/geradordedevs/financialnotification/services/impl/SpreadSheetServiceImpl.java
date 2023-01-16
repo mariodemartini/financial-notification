8ce97743e620e05ec9e58c53429173a508289892
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-16 14:38:11 -0300

    :sparkles:  feat: creating method to get the spreadsheet

--- src/main/java/br/com/geradordedevs/financialnotification/services/impl/SpreadSheetServiceImpl.java
+++ src/main/java/br/com/geradordedevs/financialnotification/services/impl/SpreadSheetServiceImpl.java
@@ -1,12 +1,23 @@
 package br.com.geradordedevs.financialnotification.services.impl;
 
+import br.com.geradordedevs.financialnotification.entites.SpreadSheetEntity;
 import br.com.geradordedevs.financialnotification.repositories.SpreadSheetRepository;
 import br.com.geradordedevs.financialnotification.services.SpreadSheetService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -20,5 +31,39 @@ public class SpreadSheetServiceImpl implements SpreadSheetService {
     public boolean validateExcelFile(MultipartFile file) {
         log.info("verify if file is a valid file");
         return Objects.equals(file.getContentType(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+    }
+
+    public static List<SpreadSheetEntity> getSpreadSheetFromExcel(InputStream inputStream){
+        List<SpreadSheetEntity> spreadSheetEntities = new ArrayList<>();
+        try {
+            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+            XSSFSheet sheet = workbook.getSheet("spreadsheet");
+            int rowIndex =0;
+            for (Row row : sheet){
+                if (rowIndex ==0){
+                    rowIndex++;
+                    continue;
+                }
+                Iterator<Cell> cellIterator = row.iterator();
+                int cellIndex = 0;
+                SpreadSheetEntity spreadSheetEntity = new SpreadSheetEntity();
+                while (cellIterator.hasNext()){
+                    Cell cell = cellIterator.next();
+                    switch (cellIndex){
+                        case 0 -> spreadSheetEntity.setMonth(cell.getStringCellValue());
+                        case 1 -> spreadSheetEntity.setInput(BigDecimal.valueOf(cell.getNumericCellValue()));
+                        case 2 -> spreadSheetEntity.setOutput(BigDecimal.valueOf(cell.getNumericCellValue()));
+                        case 3 -> spreadSheetEntity.setAmount(BigDecimal.valueOf(cell.getNumericCellValue()));
+                        default -> {
+                        }
+                    }
+                    cellIndex++;
+                }
+                spreadSheetEntities.add(spreadSheetEntity);
+            }
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+        return spreadSheetEntities;
     }
 }
