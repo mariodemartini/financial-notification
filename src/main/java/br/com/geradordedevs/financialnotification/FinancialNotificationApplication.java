1abf1ea3eabaeed402e730d02ed89a1cb61f8bfb
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-16 09:32:48 -0300

    :tada:  initial commit

---
+++ src/main/java/br/com/geradordedevs/financialnotification/FinancialNotificationApplication.java
@@ -0,0 +1,13 @@
+package br.com.geradordedevs.financialnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/geradordedevs/financialnotification/FinancialNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geradordedevs.financialnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
