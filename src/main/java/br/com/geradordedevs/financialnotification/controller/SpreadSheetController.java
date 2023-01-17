f5253f2c3226edce1d6d02da7669cfe107059159
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-17 13:27:43 -0300

    :recycle: refactor: updating controller methods

--- src/main/java/br/com/geradordedevs/financialnotification/controller/SpreadSheetController.java
+++ src/main/java/br/com/geradordedevs/financialnotification/controller/SpreadSheetController.java
@@ -1,16 +1,15 @@
 package br.com.geradordedevs.financialnotification.controller;
 
-import br.com.geradordedevs.financialnotification.entites.SpreadSheetEntity;
-import br.com.geradordedevs.financialnotification.services.SpreadSheetService;
+import br.com.geradordedevs.financialnotification.dtos.responses.SpreadSheetResponseDTO;
+import br.com.geradordedevs.financialnotification.dtos.responses.UploadExcelResponseDTO;
+import br.com.geradordedevs.financialnotification.facades.SpreadSheetFacade;
 import br.com.geradordedevs.financialnotification.services.UploadExcelService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/upload-sheet")
@@ -20,18 +19,16 @@ public class SpreadSheetController {
     private UploadExcelService uploadExcelService;
 
     @Autowired
-    private SpreadSheetService spreadSheetService;
+    private SpreadSheetFacade spreadSheetFacade;
 
     @PostMapping("/data")
-    public ResponseEntity<?> uploadCustomersData(@RequestParam("file") MultipartFile file){
-        this.spreadSheetService.saveSpreadSheetToDatabase(file);
-        return  ResponseEntity
-                .ok(Map.of("Message", "Customers data uploaded and sabe to database suceeccfully"));
+    public UploadExcelResponseDTO uploadSpreadSheet (@RequestParam("file") MultipartFile file) throws IOException {
+        return spreadSheetFacade.uploadSheet(file);
     }
 
     @GetMapping
-    public Iterable<SpreadSheetEntity> getSpreadSheets(){
-        return spreadSheetService.getSpreadSheets();
+    public List<SpreadSheetResponseDTO> getSpreadSheets(){
+        return spreadSheetFacade.getSpreadSheets();
     }
 
 }
