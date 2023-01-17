6d97890e84e6b14a2be356e6006d44752d687a7c
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-17 11:13:18 -0300

    :recycle: refactor: adding Bean from ModelMapper

--- src/main/java/br/com/geradordedevs/financialnotification/FinancialNotificationApplication.java
+++ src/main/java/br/com/geradordedevs/financialnotification/FinancialNotificationApplication.java
@@ -1,14 +1,21 @@
 package br.com.geradordedevs.financialnotification;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class FinancialNotificationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinancialNotificationApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
