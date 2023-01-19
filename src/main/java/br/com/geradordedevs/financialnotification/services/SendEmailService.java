6663109ee856b2cc27461190b5c9e2ff3ffc17af
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-19 15:51:56 -0300

    :sparkle: feat: adding email settings

--- src/main/java/br/com/geradordedevs/financialnotification/services/SendEmailService.java
+++ src/main/java/br/com/geradordedevs/financialnotification/services/SendEmailService.java
@@ -1,7 +1,9 @@
 package br.com.geradordedevs.financialnotification.services;
 
+import org.apache.commons.mail.EmailException;
+
 import java.math.BigDecimal;
 
 public interface SendEmailService {
-    void sendEmail(String month, BigDecimal amount);
+    void sendEmail(String month, BigDecimal amount) throws EmailException;
 }
