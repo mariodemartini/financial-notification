1de69afdca942f12c7ef56d1a399d24c0b77727e
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-19 21:10:21 -0300

    :recycle: refactor: updating imports

--- src/main/java/br/com/geradordedevs/financialnotification/entites/SpreadSheetEntity.java
+++ src/main/java/br/com/geradordedevs/financialnotification/entites/SpreadSheetEntity.java
@@ -1,13 +1,13 @@
 package br.com.geradordedevs.financialnotification.entites;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @AllArgsConstructor
@@ -15,6 +15,7 @@ import java.math.BigDecimal;
 @Data
 @Entity
 public class SpreadSheetEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
