b8d0759483782817b2fdde4963fe188167e140fe
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-20 10:53:53 -0300

    :recycle: refactor:  adding swagger annotation

--- src/main/java/br/com/geradordedevs/financialnotification/controller/SpreadSheetController.java
+++ src/main/java/br/com/geradordedevs/financialnotification/controller/SpreadSheetController.java
@@ -3,8 +3,11 @@ package br.com.geradordedevs.financialnotification.controller;
 import br.com.geradordedevs.financialnotification.dtos.responses.SpreadSheetResponseDTO;
 import br.com.geradordedevs.financialnotification.dtos.responses.UploadExcelResponseDTO;
 import br.com.geradordedevs.financialnotification.facades.SpreadSheetFacade;
-import br.com.geradordedevs.financialnotification.services.UploadExcelService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,14 +21,22 @@ public class SpreadSheetController {
     @Autowired
     private SpreadSheetFacade spreadSheetFacade;
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 202, message = "sent successfuly"),
+            @ApiResponse(code = 400, message = "bad request"),
+    })
     @PostMapping("/data")
-    public UploadExcelResponseDTO uploadSpreadSheet (@RequestParam("file") MultipartFile file) throws IOException {
-        return spreadSheetFacade.uploadSheet(file);
+    public ResponseEntity<UploadExcelResponseDTO> uploadSpreadSheet (@RequestParam("file") MultipartFile file) throws IOException {
+        return new ResponseEntity<>(spreadSheetFacade.uploadSheet(file), HttpStatus.ACCEPTED);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "find all successfuly"),
+            @ApiResponse(code = 400, message = "bad request")
+    })
     @GetMapping
-    public List<SpreadSheetResponseDTO> getSpreadSheets(){
-        return spreadSheetFacade.getSpreadSheets();
+    public ResponseEntity<List<SpreadSheetResponseDTO>> getSpreadSheets(){
+        return new ResponseEntity<>(spreadSheetFacade.getSpreadSheets(), HttpStatus.OK);
     }
 
 }
