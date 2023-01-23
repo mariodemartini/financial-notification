cb424ee82cd57e024311f572bfb5000f8be45190
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-01-23 14:30:04 -0300

    :white_check_mark: test:  creating test for list method of facade

---
+++ src/test/java/br/com/geradordedevs/financialnotification/facades/impl/SpreadSheetFacadeImplTest.java
@@ -0,0 +1,82 @@
+package br.com.geradordedevs.financialnotification.facades.impl;
+
+import br.com.geradordedevs.financialnotification.dtos.responses.SpreadSheetResponseDTO;
+import br.com.geradordedevs.financialnotification.entites.SpreadSheetEntity;
+import br.com.geradordedevs.financialnotification.mappers.SpreadSheetMapper;
+import br.com.geradordedevs.financialnotification.services.SendEmailService;
+import br.com.geradordedevs.financialnotification.services.SpreadSheetService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+@SpringBootTest
+public class SpreadSheetFacadeImplTest {
+
+    @InjectMocks
+    private SpreadSheetFacadeImpl spreadSheetFacade;
+
+    @Mock
+    private SpreadSheetService spreadSheetService;
+
+    @Mock
+    private SendEmailService sendEmailService;
+
+    @Mock
+    private SpreadSheetMapper spreadSheetMapper;
+
+    private static final Long MOCK_ID = 1L;
+    private static final String MOCK_MONTH = "July";
+    private static final BigDecimal MOCK_INPUT = new BigDecimal(100.00);
+    private static final BigDecimal MOCK_OUTPUT = new BigDecimal(50.00);
+    private static final BigDecimal MOCK_AMOUNT = new BigDecimal(50.00);
+
+    @Before
+    public void setupMock(){
+        MockitoAnnotations.openMocks(this);
+        when(spreadSheetService.getSpreadSheets()).thenReturn(returnListSpreadSheetEntities());
+
+        when(spreadSheetMapper.toListDTO(returnListSpreadSheetEntities())).thenReturn(returnListSpreadSheetDTO());
+    }
+
+    @Test
+    public void getSpreadSheetsShouldReturnOk() throws Exception{
+        assertEquals(returnListSpreadSheetDTO(), spreadSheetFacade.getSpreadSheets());
+    }
+
+
+
+    private List<SpreadSheetResponseDTO> returnListSpreadSheetDTO() {
+        List<SpreadSheetResponseDTO> list = new ArrayList<>();
+        list.add(returnSpreadSheetResponseDTO());
+
+        return list;
+    }
+
+    private SpreadSheetResponseDTO returnSpreadSheetResponseDTO() {
+        return new SpreadSheetResponseDTO(MOCK_MONTH, MOCK_INPUT, MOCK_OUTPUT, MOCK_AMOUNT);
+    }
+
+    private List<SpreadSheetEntity> returnListSpreadSheetEntities() {
+        List<SpreadSheetEntity> list = new ArrayList<>();
+        list.add(returnSpreadSheetEntity());
+
+        return list;
+    }
+
+    private SpreadSheetEntity returnSpreadSheetEntity() {
+        return new SpreadSheetEntity(MOCK_ID, MOCK_MONTH, MOCK_INPUT, MOCK_OUTPUT, MOCK_AMOUNT);
+    }
+}
